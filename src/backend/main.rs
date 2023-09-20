ac04ff5c0707bdd8df81a7d84e4e38ade1fd9554
Philipp Otto <p.otto94@gmx.de>
2023-09-20 21:10:11 +0200

    Apply formatting

--- src/backend/main.rs
+++ src/backend/main.rs
@@ -1,10 +1,17 @@
 use axum::{
+    extract::Query,
+    extract::State,
+    response::Html,
     routing::{get, post},
-    Router, response::Html, extract::Query, extract::{State, FromRef}
+    Router,
 };
-use std::{net::SocketAddr, ops::RangeTo, rc::Rc, sync::{RwLock, Mutex, Arc}, collections::HashMap};
-use serde::{Serialize, Deserialize};
-use minijinja::{Environment, render, context};
+use minijinja::{context, Environment};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, Mutex, RwLock},
+};
 use tower_http::services::ServeDir;
 
 #[derive(Clone)]
@@ -32,26 +39,26 @@ struct CategoryRequest {
 
 fn get_lines(u32: u64, hint_nr: u32) -> Vec<Line> {
     let lines = vec![
-        Line{
+        Line {
             category: "Peak rating".to_string(),
-            answer: "2882".to_string()
+            answer: "2882".to_string(),
         },
-        Line{
+        Line {
             category: "Birth date".to_string(),
-            answer: "30 November 1990".to_string()
+            answer: "30 November 1990".to_string(),
         },
-        Line{
+        Line {
             category: "dasdsa".to_string(),
-            answer: "1".to_string()
+            answer: "1".to_string(),
         },
-        Line{
+        Line {
             category: "asdasda".to_string(),
-            answer: "2".to_string()
+            answer: "2".to_string(),
         },
-        Line{
+        Line {
             category: "asdasdas".to_string(),
-            answer: "3".to_string()
-        }
+            answer: "3".to_string(),
+        },
     ];
 
     let last_hint = std::cmp::min(hint_nr as usize, lines.len());
@@ -64,13 +71,11 @@ async fn introduction(State(state): State<Arc<RwLock<AppState<'_>>>>) -> Html<St
     let app_state = state.read().unwrap();
 
     let template = app_state.env.get_template("game").unwrap();
-    let rendered = template.render(
-        context!(start_screen => true)
-    );
+    let rendered = template.render(context!(start_screen => true));
 
     match rendered {
         Ok(result) => Html(result),
-        Err(..) => Html("".into())
+        Err(..) => Html("".into()),
     }
 }
 
@@ -78,29 +83,28 @@ async fn start_game(State(state): State<Arc<RwLock<AppState<'_>>>>) -> Html<Stri
     let app_state = state.read().unwrap();
 
     let template = app_state.env.get_template("game").unwrap();
-    let rendered = template.render(
-        context!(lines => get_lines(0, 1), start_screen => false)
-    );
+    let rendered = template.render(context!(lines => get_lines(0, 1), start_screen => false));
 
     match rendered {
         Ok(result) => Html(result),
-        Err(..) => Html("".into())
+        Err(..) => Html("".into()),
     }
 }
 
-async fn get_category(State(state): State<Arc<RwLock<AppState<'_>>>>, request: Query<CategoryRequest>) -> Html<String> {
+async fn get_category(
+    State(state): State<Arc<RwLock<AppState<'_>>>>,
+    request: Query<CategoryRequest>,
+) -> Html<String> {
     // println!("request get_category with id {}", request.id);
 
     let app_state = state.read().unwrap();
 
     let template = app_state.env.get_template("playarea").unwrap();
-    let rendered = template.render(
-        context!(lines => get_lines(0, request.id))
-    );
+    let rendered = template.render(context!(lines => get_lines(0, request.id)));
 
     match rendered {
         Ok(result) => Html(result),
-        Err(..) => Html("".into())
+        Err(..) => Html("".into()),
     }
 }
 
@@ -111,17 +115,23 @@ async fn main() {
         game_states: Arc::new(HashMap::new()),
     };
 
-    state.env.add_template("game", include_str!("../../html/game.html")).expect("Could not load a template!");
-    state.env.add_template("playarea", include_str!("../../html/playarea.html")).expect("Could not load a template!");
+    state
+        .env
+        .add_template("game", include_str!("../../html/game.html"))
+        .expect("Could not load a template!");
+    state
+        .env
+        .add_template("playarea", include_str!("../../html/playarea.html"))
+        .expect("Could not load a template!");
 
     let shared_state = Arc::new(RwLock::new(state));
 
     let app = Router::new()
-    .route("/", get(introduction))
-    .route("/category", get(get_category))
-    .route("/start_game", get(start_game))
-    .nest_service("/assets", ServeDir::new("assets"))
-    .with_state(shared_state);
+        .route("/", get(introduction))
+        .route("/category", get(get_category))
+        .route("/start_game", get(start_game))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .with_state(shared_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}...", addr);
